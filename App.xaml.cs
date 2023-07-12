994d89451ddfdfdcffd8a1cba58168c77d55af0e
DecayingEagle <46646694+DecayingEagle@users.noreply.github.com>
2023-07-12 04:06:38 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SnekAI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SnekAI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
